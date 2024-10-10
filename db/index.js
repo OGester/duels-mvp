c137df1742ff87da6f30c5d78cc10fbce894bead
Oskar Gester <gester.oskar@gmail.com>
2024-10-10 15:43:37 +0200

    Migrated database to supabase, fixed bug in database instance.

--- db/index.js
+++ db/index.js
@@ -1,3 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
-export const db = new PrismaClient();
+export const db = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") global.prisma = db;
+
+//import { PrismaClient } from "@prisma/client";
+//export const db = new PrismaClient();
