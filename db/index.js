517c05d36fe62abe5445ab7bdf3249694891f3f5
Oskar Gester <gester.oskar@gmail.com>
2024-10-14 10:10:30 +0200

    changed to ENV === "production" in db instance

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 export const db = global.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") global.prisma = db;
+if (process.env.NODE_ENV === "production") global.prisma = db;
 
 //import { PrismaClient } from "@prisma/client";
 //export const db = new PrismaClient();
