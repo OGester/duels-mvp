16f0da498d60653cc5fa194cb7282a59bc8469ed
Oskar Gester <gester.oskar@gmail.com>
2024-10-14 10:01:22 +0200

    inserted database strin into prisma achema

--- app/profile-page/page.jsx
+++ app/profile-page/page.jsx
@@ -2,7 +2,7 @@ import SignOutButton from "@/components/SignOutButton";
 import { getUserFromSession } from "@/lib/auth";
 
 export default async function userPage() {
-  const user = await getUserFromSession();
+  //const user = await getUserFromSession();
   //console.log(user);
 
   return (
