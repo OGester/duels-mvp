6c0aa7e2d7754fdbd8f96e117ec4676aa916b7d3
Oskar Gester <gester.oskar@gmail.com>
2024-11-05 10:29:04 +0100

    started crud operations for league without a new branch, commiting and creting that branch

--- app/leagues/page.jsx
+++ app/leagues/page.jsx
@@ -1,17 +1,26 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { db } from "@/db";
 
 export const metadata = {
   title: "Leagues",
 };
 
 export default async function LeaguesPage() {
+  const leagues = await db.league.findMany();
+
+  //rendering the array of leagues fetched from the database
+  const renderedLeagues = leagues.map((league) => {
+    return <div key={league.league_id}>{league.name}</div>;
+  });
+
   return (
     <>
       <main className="flex flex-col w-full">
         <div className="flex-auto justify-center m-2">
           <h2 className="text-center">Leagues</h2>
         </div>
+        <div className="flex flex-col py-2">{renderedLeagues}</div>
         <div className="flex justify-center m-2">
           <Button asChild className="bg-orange-300 text-black">
             <Link href="/leagues/new">Create new League</Link>
