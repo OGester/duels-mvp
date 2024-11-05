80013bd37fa1b066e0a46b8c67b69c11781221fc
Oskar Gester <gester.oskar@gmail.com>
2024-11-05 11:08:33 +0100

    leagues displayed linked to the league page

--- app/leagues/page.jsx
+++ app/leagues/page.jsx
@@ -11,16 +11,29 @@ export default async function LeaguesPage() {
 
   //rendering the array of leagues fetched from the database
   const renderedLeagues = leagues.map((league) => {
-    return <div key={league.league_id}>{league.name}</div>;
+    return (
+      <Link
+        key={league.league_id}
+        href={`/leagues/${league.league_id}`}
+        className="flex justify-between items-center p-2 border border-orange-300 rounded"
+      >
+        <div>{league.name}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
   return (
     <>
-      <main className="flex flex-col w-full">
+      <main className="flex justify-center flex-col w-full">
         <div className="flex-auto justify-center m-2">
           <h2 className="text-center">Leagues</h2>
         </div>
-        <div className="flex flex-col py-2">{renderedLeagues}</div>
+        <div className="flex justify-center m-2">
+          <div className="flex flex-col w-1/2 py-2 gap-2">
+            {renderedLeagues}
+          </div>
+        </div>
         <div className="flex justify-center m-2">
           <Button asChild className="bg-orange-300 text-black">
             <Link href="/leagues/new">Create new League</Link>
