3decdffddebf8b09533e7b7664a7d735986235a5
Oskar Gester <gester.oskar@gmail.com>
2024-11-08 11:17:24 +0100

    implemented debounceon search

--- components/SearchLeague.jsx
+++ components/SearchLeague.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { Input } from "@/components/ui/input";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function SearchLeague() {
   const searchParams = useSearchParams();
@@ -9,19 +10,21 @@ export default function SearchLeague() {
 
   const router = useRouter();
 
-  const handleChange = (query) => {
+  const handleChange = useDebouncedCallback((query) => {
     const params = new URLSearchParams(searchParams);
 
     if (query) params.set("query", query);
     else params.delete("query");
 
     router.replace(`${pathName}?${params.toString()}`);
-  };
+    //setting the timout to 1s for the debouncecallback
+  }, 1000);
 
   return (
     <div>
       <Input
         onChange={(e) => handleChange(e.target.value)}
+        defaultValue={searchParams.get("query") ?? ""}
         placeholder="Search"
       />
     </div>
