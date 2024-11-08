2d54f68be04ae5ec6a4882775a0ef07a876260f7
Oskar Gester <gester.oskar@gmail.com>
2024-11-08 15:01:33 +0100

    got pagination working with show more an back links when showing leagues

--- app/leagues/page.jsx
+++ app/leagues/page.jsx
@@ -15,28 +15,33 @@ export default async function LeaguesPage({ searchParams }) {
   //if the query is undefined its set to en empty string
   const query = searchParams.query ?? "";
 
-  const PAGE_SIZE = 10;
+  const PAGE_SIZE = 5;
 
   const leagues = await db.league.findMany({
     where: {
       name: { contains: query, mode: "insensitive" },
     },
     skip: +pagenum * PAGE_SIZE,
-    take: PAGE_SIZE,
+    //fetch one extra item to know if there is more
+    take: PAGE_SIZE + 1,
   });
 
   //getting the total count of leagues in DB that match the search
-  const totalLeagues = db.league.count({
+  const totalLeagues = await db.league.count({
     where: {
       name: { contains: query, mode: "insensitive" },
     },
   });
 
-  //calculates if there is more pages to load
-  const showMore = (pagenum + 1) * PAGE_SIZE < totalLeagues;
+  //determine if there is an extra league, and in that case an extra page
+  const showMore = leagues.length > PAGE_SIZE;
+  //if the page number is larger than 0 (first page)
+  const goBack = pagenum > 0;
 
   //rendering the array of leagues fetched from the database
-  const renderedLeagues = leagues.map((league) => {
+  //slice will only show the first PAGE_SIZE leagues but still
+  //allow to know if there is more to show
+  const renderedLeagues = leagues.slice(0, PAGE_SIZE).map((league) => {
     return (
       <Link
         key={league.league_id}
@@ -66,25 +71,28 @@ export default async function LeaguesPage({ searchParams }) {
         <div className="flex flex-col w-1/2 py-2 gap-2">{renderedLeagues}</div>
       </div>
       <div className="flex justify-center gap-4 m-4">
-        <Link
-          href={{
-            pathname: "/leagues",
-            query: { query, pagenum: Number(pagenum) + 1 },
-          }}
-          className="font-semibold text-orange-300"
-        >
-          Show more
-        </Link>
-
-        <Link
-          href={{
-            pathname: "/leagues",
-            query: { query, pagenum: Number(pagenum) - 1 },
-          }}
-          className="font-semibold text-orange-300"
-        >
-          Back
-        </Link>
+        {goBack && (
+          <Link
+            href={{
+              pathname: "/leagues",
+              query: { query, pagenum: Number(pagenum) - 1 },
+            }}
+            className="font-semibold text-orange-300"
+          >
+            Back
+          </Link>
+        )}
+        {showMore && (
+          <Link
+            href={{
+              pathname: "/leagues",
+              query: { query, pagenum: Number(pagenum) + 1 },
+            }}
+            className="font-semibold text-orange-300"
+          >
+            Show more
+          </Link>
+        )}
       </div>
     </main>
   );
