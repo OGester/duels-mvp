7ecd5e63ff9fab644652175d6c32aadae3097931
Oskar Gester <gester.oskar@gmail.com>
2024-11-08 12:56:43 +0100

    pagination with 10 leagues per page

--- app/leagues/page.jsx
+++ app/leagues/page.jsx
@@ -2,19 +2,38 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { db } from "@/db";
 import SearchLeague from "@/components/SearchLeague";
+import { skip } from "@prisma/client/runtime/library";
 
 export const metadata = {
   title: "Leagues",
 };
 
 export default async function LeaguesPage({ searchParams }) {
+  //if the pagenum is undefined its set to 0
+  const pagenum = searchParams.pagenum ?? 0;
+
+  //if the query is undefined its set to en empty string
   const query = searchParams.query ?? "";
+
+  const PAGE_SIZE = 10;
 
   const leagues = await db.league.findMany({
     where: {
       name: { contains: query, mode: "insensitive" },
     },
+    skip: +pagenum * PAGE_SIZE,
+    take: PAGE_SIZE,
   });
+
+  //getting the total count of leagues in DB that match the search
+  const totalLeagues = db.league.count({
+    where: {
+      name: { contains: query, mode: "insensitive" },
+    },
+  });
+
+  //calculates if there is more pages to load
+  const showMore = (pagenum + 1) * PAGE_SIZE < totalLeagues;
 
   //rendering the array of leagues fetched from the database
   const renderedLeagues = leagues.map((league) => {
@@ -46,11 +65,27 @@ export default async function LeaguesPage({ searchParams }) {
       <div className="flex justify-center m-2">
         <div className="flex flex-col w-1/2 py-2 gap-2">{renderedLeagues}</div>
       </div>
-      {/*   <div className="flex justify-center m-2">
-        <Button asChild className="bg-orange-300 text-black">
-          <Link href="/leagues/new">Create new League</Link>
-        </Button>
-      </div> */}
+      <div className="flex justify-center gap-4 m-4">
+        <Link
+          href={{
+            pathname: "/leagues",
+            query: { query, pagenum: Number(pagenum) + 1 },
+          }}
+          className="font-semibold text-orange-300"
+        >
+          Show more
+        </Link>
+
+        <Link
+          href={{
+            pathname: "/leagues",
+            query: { query, pagenum: Number(pagenum) - 1 },
+          }}
+          className="font-semibold text-orange-300"
+        >
+          Back
+        </Link>
+      </div>
     </main>
   );
 }
