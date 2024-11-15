9e2cb2e553d84c42bcdf8ef34e1d20a762f9f539
Oskar Gester <gester.oskar@gmail.com>
2024-11-15 12:53:04 +0100

    setting role in league user according to if user is owner or admin, otherwise is set to user

--- components/JoinLeagueButton.jsx
+++ components/JoinLeagueButton.jsx
@@ -3,15 +3,22 @@ import { redirect } from "next/navigation";
 import { joinLeague } from "@/lib/league";
 //import { joinLeagueTest } from "@/lib/league";
 import { getUserFromSession } from "@/lib/auth";
+import { getLeagueRole } from "@/lib/league";
 
 export default function JoinLeagueButton({ league }) {
   async function action() {
     "use server";
+    //gets the logged in users user_id
     const user = await getUserFromSession();
     const user_id = user.user_id;
+
+    //gets league id from the leaguePage props
     const league_id = league.league_id;
 
-    joinLeague(user_id, league_id);
+    //gets the joining users league_user_role
+    const leagueRole = await getLeagueRole(user_id, league_id);
+
+    joinLeague(user_id, league_id, leagueRole);
     //test if the specific league route could be reached from this component
     redirect(`/leagues/${league.league_id}`);
     //redirect(`/leagues`);
