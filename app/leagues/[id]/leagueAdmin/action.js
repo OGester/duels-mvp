5fa0c96a0ee17b57fed4fc3bc943c503c7d870a3
Oskar Gester <gester.oskar@gmail.com>
2024-11-27 15:14:16 +0100

    fixed bug with showing league_user_role on rendered leagues on profile_page

--- app/leagues/[id]/leagueAdmin/action.js
+++ app/leagues/[id]/leagueAdmin/action.js
@@ -10,12 +10,18 @@ export async function setLeagueAdminAction(formData) {
   const email = formData.get("userEmail");
   console.log("User email entered:", email);
 
+  //error handled in findUser function
   const user = await findUser(email);
 
   //console.log("User found:", user);
 
   const user_id = user.user_id;
   const league_id = formData.get("league_id");
+  //make sure there is a league_id provided
+  if (!league_id) {
+    console.error("league id is missing");
+    return { error: "League Id is required" };
+  }
   const role = "ADMIN";
 
   const data = {
@@ -24,7 +30,16 @@ export async function setLeagueAdminAction(formData) {
     role,
   };
 
-  setLeagueAdmin(data);
+  try {
+    await setLeagueAdmin(data);
+  } catch (error) {
+    //handle errors from setting the league admin
+    console.error("Error setting league admin:", error);
+    return { error: "Failed to assign admin role" };
+  }
+  //setLeagueAdmin(data);
+
+  //redirects if successfull
 
   redirect("/leagues");
 }
