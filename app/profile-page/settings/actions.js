8ecfe63d4e02632a1999ea235c077240c6450ac3
Oskar Gester <gester.oskar@gmail.com>
2024-11-27 11:47:16 +0100

    simple URL validation, should be more precice in a Full production version.

--- app/profile-page/settings/actions.js
+++ app/profile-page/settings/actions.js
@@ -6,10 +6,38 @@ import { getUserFromSession } from "@/lib/auth";
 export async function updateProfileAction(formData) {
   const user = await getUserFromSession();
   //console.log("[signInAction]", formData);
+  //retrieving data from formData
+  const profile_image_url = formData.get("profileImageUrl");
+  const description = formData.get("description");
+
+  //make sure description is not longer than 500 characters
+  if (description && description.length > 500) {
+    return {
+      isError: true,
+      message: "Description is to long!",
+    };
+  }
+
+  if (profile_image_url) {
+    console.log("[updateProfileAction] Validating URL:", profile_image_url);
+    //remove extra spaces taht might interfere with startsWith check
+    //might not be a strong and forever validation but good enough for this MVP
+    const trimmedUrl = profile_image_url.trim();
+    if (
+      !trimmedUrl.startsWith("http://") &&
+      !trimmedUrl.startsWith("https://")
+    ) {
+      return {
+        isError: true,
+        message: "Invalid picture URL! please try again",
+      };
+    }
+  }
+
   const data = {
     user_id: user.user_id,
-    profile_image_url: formData.get("profileImageUrl"),
-    description: formData.get("description"),
+    profile_image_url: profile_image_url,
+    description: description,
   };
   //console.log("Data sent to updateProfile:", data);
   //console.log("the session user is:", user);
